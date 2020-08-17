7e8655f075360a76f4f512c520d2f6baff7c9b6a
Wolfram Huesken <wolfram.huesken@global-fashion-group.com>
2020-08-17 20:39:58 +0200

    Another change in c9a85d17 takes away the chance for individual changes. I think DRY can be ignored here, because our goal is to preserve the original as much as possible

--- library/Zend/Date.php
+++ library/Zend/Date.php
@@ -1818,7 +1818,6 @@ class Zend_Date extends Zend_Date_DateObject
                 require_once 'Zend/Date/Exception.php';
                 throw new Zend_Date_Exception("invalid date ($date) operand, swatchstamp expected", 0, null, $date);
 
-            case self::HOUR_SHORT:
             case self::HOUR_SHORT_AM:
                 if (is_numeric($date)) {
                     return $this->_assign($calc, $this->mktime((int)$date, 0, 0, 1, 1, 1970, true),
@@ -1827,6 +1826,16 @@ class Zend_Date extends Zend_Date_DateObject
 
                 require_once 'Zend/Date/Exception.php';
                 throw new Zend_Date_Exception("invalid date ($date) operand, hour expected", 0, null, $date);
+
+            case self::HOUR_SHORT:
+                if (is_numeric($date)) {
+                    return $this->_assign($calc, $this->mktime((int)$date, 0, 0, 1, 1, 1970, true),
+                                                 $this->mktime($hour,         0, 0, 1, 1, 1970, true), false);
+                }
+
+                require_once 'Zend/Date/Exception.php';
+                throw new Zend_Date_Exception("invalid date ($date) operand, hour expected", 0, null, $date);
+                break;
 
             case self::HOUR_AM:
                 if (is_numeric($date)) {
